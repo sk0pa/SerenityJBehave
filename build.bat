0802c86620cfdd3b734dda441e8cc9b2f704606a
Anhelina Khalii <anhelina_khalii@epam.com>
2015-10-29 10:46:47 +0200

    Replace chromedriver to another location

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-mvn verify -Dwebdriver.chrome.driver=src\main\java\BDDApproach\resources\chromedriver.exe -Dwebdriver.driver=chrome && start target\site\serenity\index.html
+mvn verify -Dwebdriver.chrome.driver=src\main\java\resources\chromedriver.exe -Dwebdriver.driver=chrome && start target\site\serenity\index.html
